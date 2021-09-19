ad6595465a02f4c4549c7b932602d37bfd6da0e7
Sakura-tyan-meow <90412520+Sakura-tyan-meow@users.noreply.github.com>
2021-09-19 10:48:41 +0300

    вариант 23

---
+++ dz1/dz1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace dz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("введите число");
+            
+            int m = Convert.ToInt32(Console.ReadLine());
+            int a = m % 10000 / 1000;
+            Console.WriteLine( $"разряд тысяч : {a}");
+        }
+    }
+}
